aeefc207aa7589cc47b0cd1477d4ee73724e95ae
alexdevsprint <alexdevsprint@gmail.com>
2025-04-30 14:41:19 +0300

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,13 @@
 import css from "./Contact.module.css";
+import { BiSolidPhone } from "react-icons/bi";
+import { BiSolidUser } from "react-icons/bi";
 
 export default function Contact({ name, number }) {
   return (
     <div className={css.contactCard}>
       <ul className={css.contactList}>
-        <li>{name}</li>
-        <li>{number}</li>
+        <li className={css.contactItem}><BiSolidUser /><span>{name}</span></li>
+        <li className={css.contactItem}><BiSolidPhone /><span>{number}</span></li>
       </ul>
       <button>Delete</button>
     </div>
