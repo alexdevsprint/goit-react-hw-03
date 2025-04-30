9a9c57afe018427ce01cc3093140aa2ac0ac7f50
alexdevsprint <alexdevsprint@gmail.com>
2025-04-30 10:48:28 +0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import css from "./App.module.css";
+
+import Contact from "./components/Contact/Contact";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,8 @@
+import css from "./ContactList.module.css"
+
+export default function ContactList(){
+    return (
+        <>
+        </>
+    )
+}
